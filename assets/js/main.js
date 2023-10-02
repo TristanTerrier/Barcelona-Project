232c358f213d8ec1b8a055070a079ac7088ab415
Billy Rogier <billy.rogier@free.fr>
2023-10-02 14:43:22 +0200

    starting

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+import { Table } from './modules/Table.js'
+
+const ProductsTable = new Table('http://localhost:3000', 'products')
+
+// ProductsTable.findAll().then((data) => {
+
+// })
+
+// ProductsTable.findOneBy('id', 1)
---
+++ assets/js/modules/Table.js
@@ -0,0 +1,20 @@
+export class Table {
+    constructor(base_url, table_name) {
+        this.base_url = base_url
+        this.table_name = table_name
+    }
+
+    async findAll() {
+        const data = await fetch(`${this.base_url}/${this.table_name}`).then(
+            (response) => response.json()
+        )
+        return data
+    }
+
+    async findOneBy(key, value) {
+        const data = await fetch(
+            `${this.base_url}/${this.table_name}?${key}=${value}`
+        ).then((response) => response.json())
+        return data
+    }
+}
